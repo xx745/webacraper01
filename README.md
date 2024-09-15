3ca36a9246ac4a5942add1e3377efcd0c0d208ec
xx745 <xx745>
2024-09-15 20:46:33 +0200

    added playwright and basic function to test if it's working

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # webscraper01
+
+```bash
+npm install
+npx playwright install-deps
+npx playwright install chromium
+node index.js
+```
---
+++ index.js
@@ -0,0 +1,11 @@
+const playwright = require('playwright');
+
+(async () => {
+  const browser = await playwright.chromium.launch();
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  await page.goto("https://amazon.com");
+  await page.screenshot({ path: `amazon_webpage.png`, fullPage: true });
+  await page.waitForTimeout(1000);
+  await browser.close();
+})();
